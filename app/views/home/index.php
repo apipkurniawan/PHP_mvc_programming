e1fc37aba13323eb06dc5b89ca5068c5e45cb7ca
apip kurniawan <33179999+apipkurniawan@users.noreply.github.com>
2019-03-27 17:16:53 +0700

    MVC

---
+++ app/views/home/index.php
@@ -0,0 +1,14 @@
+<div class="container">
+	<div class="jumbotron mt-4">
+	  <h1 class="display-4" style="text-align: center;">selamat datang di Aplikasi Penggajian</h1>
+	  <br>
+	  <p class="lead" style="color: red;"><i>Dibuat oleh apip kurniawan</i></p>
+	  <hr class="my-4">
+	  <h3>Manfaat Aplikasi : </h3>
+	  <br>
+	  <p class="lead">Bisa menghitung gaji karyawan dengan mudah tanpa menggunakan dekstop.</p>
+	  <p class="lead">Bisa dilakukan dimana saja dan kapan saja.</p>
+	  <p class="lead">Semua data terintegrasi dengan baik.</p>
+	  <p class="lead">Database tersimpan di server.</p>
+	</div>
+</div>
