256eadc780e82c964ac4539ad5664ba821940996
apip kurniawan <33179999+apipkurniawan@users.noreply.github.com>
2019-03-27 16:54:23 +0700

    MVC

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+// menentukan url dasar
+define('BASEURL', 'http://localhost/phpmvc/public');
+
+// konek Database
+define('DB_HOST', 'localhost'); 
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc'); // phpmvc adalah nama database
